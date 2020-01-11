a8904efdc36cffbec935e15afe06294882232780
lil206chris <big206chris@gmail.com>
2020-01-11 12:32:11 -0800

    joysticks

---
+++ src/main/java/competition/subsystems/MotorControl/commands/MotorControlJoysticks.java
@@ -0,0 +1,34 @@
+package competition.subsystems.MotorControl.commands;
+
+import com.google.inject.Inject;
+
+import competition.operator_interface.OperatorInterface;
+import competition.subsystems.MotorControl.MotorControl21SubSystem;
+import competition.subsystems.MotorControl.MotorControl22SubSystem;
+import xbot.common.command.BaseCommand;
+
+public class MotorControlJoysticks extends BaseCommand
+{
+    final MotorControl21SubSystem motor21;
+    final MotorControl22SubSystem motor22;
+    final OperatorInterface oi;
+
+    public MotorControlJoysticks(OperatorInterface oi,
+                                 Mor)
+    {
+        
+    }
+
+    @Override
+    public void initialize() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
